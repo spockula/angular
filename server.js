bc01ac2bd8f46b3c5e221ad47fc7d26785edc907
spockula <chinedukogu@gmail.com>
2019-11-08 15:50:51 +0000

    testing

--- server.js
+++ server.js
@@ -4,13 +4,5 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/people-profile-web'));
-
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/people-profile-web/index.html'));
-});
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
