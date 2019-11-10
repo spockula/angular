b065a96343ce1a58f3844fbeb6b67e1cbad56ec4
spockula <chinedukogu@gmail.com>
2019-11-10 22:51:47 +0000

    testing

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(${__dirname}/front-end/dist/));
+app.use(express.static(__dirname + '/front-end/dist/));
 
 app.get('*', (req, res) => {
   res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
