3d487bc988329b6e06a1c86cb0027198cc830dfe
spockula <chinedukogu@gmail.com>
2020-02-16 20:50:29 +0100

    update

--- src/app/request-password/request-password.component.ts
+++ src/app/request-password/request-password.component.ts
@@ -43,6 +43,7 @@ export class RequestPasswordComponent implements OnInit {
             registerForm.resetForm();
             this.FormValue = registerForm.value;
            this.successSnackBar();
+           this.router.navigate(['/login']);
             this.ngxService.stop();
           }
         );
